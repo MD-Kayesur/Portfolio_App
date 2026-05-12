cb4dd12c725c88d870700a31f1f5f2c8ea00a3c3
MD-Kayesur <rmdkayesur@gmail.com>
2026-05-12 14:08:47 +0600

    update environment variables and base API URL to production server endpoints

--- store/baseApi.ts
+++ store/baseApi.ts
@@ -4,7 +4,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const baseApi = createApi({
   reducerPath: 'baseApi',
   baseQuery: fetchBaseQuery({
-    baseUrl: process.env.EXPO_PUBLIC_API_URL || 'https://nur-jahan-fevrics-server.vercel.app',
+    baseUrl: process.env.EXPO_PUBLIC_API_URL || 'https://my-own-portfolio-server.vercel.app',
   }),
   tagTypes: ['Blogs', 'Users', 'Projects'],
   endpoints: () => ({}),
